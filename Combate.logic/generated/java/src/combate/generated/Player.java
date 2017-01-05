ea5fb8b19733285096323e92464d459d2cd63904
killghost15 <killghost15@gmail.com>
2017-01-05 03:54:54 +0000

    new player

--- Combate.logic/generated/java/src/combate/generated/Player.java
+++ Combate.logic/generated/java/src/combate/generated/Player.java
@@ -5,35 +5,18 @@ import org.overture.codegen.runtime.*;
 
 @SuppressWarnings("all")
 public class Player {
-  public static final Number TOTAL_PIECES = 40L;
   private Number player = 0L;
-  private Number totalPieces = Player.TOTAL_PIECES;
-  private Number unplayedPieces = Player.TOTAL_PIECES;
-  private VDMSet pieces = SetUtil.set();
+  private Object color;
 
-  public void cg_init_Player_1(final Number pl) {
+  public void cg_init_Player_1(final Number pl, final Object color1) {
 
     player = pl;
+    color = color1;
   }
 
-  public Player(final Number pl) {
+  public Player(final Number pl, final Object color1) {
 
-    cg_init_Player_1(pl);
-  }
-
-  public void addPiece(final Piece p) {
-
-    pieces = SetUtil.union(SetUtil.set(p), Utils.copy(pieces));
-  }
-
-  public VDMSet getPieces() {
-
-    return Utils.copy(pieces);
-  }
-
-  public void eliminatePiece(final Piece p) {
-
-    pieces = SetUtil.diff(Utils.copy(pieces), SetUtil.set(p));
+    cg_init_Player_1(pl, color1);
   }
 
   public Player() {}
@@ -41,16 +24,10 @@ public class Player {
   public String toString() {
 
     return "Player{"
-        + "TOTAL_PIECES = "
-        + Utils.toString(TOTAL_PIECES)
-        + ", player := "
+        + "player := "
         + Utils.toString(player)
-        + ", totalPieces := "
-        + Utils.toString(totalPieces)
-        + ", unplayedPieces := "
-        + Utils.toString(unplayedPieces)
-        + ", pieces := "
-        + Utils.toString(pieces)
+        + ", color := "
+        + Utils.toString(color)
         + "}";
   }
 }
